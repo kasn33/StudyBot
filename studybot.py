2fb8ec86ecda2e3c86cae6b6dc5ac82b538994ba
kasn33 <112991386+kasn33@users.noreply.github.com>
2024-04-12 16:14:02 -0500

    Basic study hour tracking and checking

--- studybot.py
+++ studybot.py
@@ -1,85 +1,131 @@
 import discord
+from discord.ext import commands, tasks
 import asyncio
-from discord.ext import commands
-from discord import app_commands #for slash commands if they ever work
-from dataclasses import dataclass
+import datetime
+import sqlite3
 
-BOT_TOKEN = NULL #add back in when using
+BOT_TOKEN = ""
 CHANNEL_ID = 1227722112404553889
 SERVER_ID = 1227721401239343187
 intents = discord.Intents.all()
 client = discord.Client(intents=intents)
-tree = app_commands.CommandTree(client)
-
-
-@dataclass
-class Session:
-    is_active: bool = False
-    start_time: int = 0
 
 bot = commands.Bot(command_prefix="!", intents=intents)
-session = Session()
+
+#initialize / connect database
+con = sqlite3.connect("hours.db")
+cur = con.cursor()
 
 
 #startup message (console and channel in server)
 @bot.event
 async def on_ready():
-    print("Hello! Study bot is ready!")
+    cur.execute("""CREATE TABLE IF NOT EXISTS studiers(
+        user_id INTEGER PRIMARY KEY,
+        total_time INTEGER
+    );""")
+    cur.execute("""CREATE TABLE IF NOT EXISTS sessions(
+        session_id INTEGER PRIMARY KEY AUTOINCREMENT,
+        user_id INTEGER,
+        start_time INTEGER,
+        is_complete INTEGER DEFAULT 0,
+        duration INTEGER,
+        activity TEXT,
+        FOREIGN KEY (user_id) REFERENCES studiers(user_id)
+    );""")
+    con.commit()
+    print("Study bot is ready!")
     channel = bot.get_channel(CHANNEL_ID)
     await channel.send("Hello! Study bot is ready!")
 
+
+
+
 ########### Study tracker functions ###############
 
-@bot.command()
+@bot.slash_command(description = "Start your study session")
 async def start(ctx):
-    if session.is_active:
-        await ctx.send("Session already active!")
+    user_id = int(ctx.author.id)
+    check_studier = cur.execute(f"SELECT total_time FROM studiers WHERE user_id = (?)", (user_id,)).fetchone()
+    if not check_studier:
+        cur.execute(f"INSERT INTO studiers VALUES (?,?)", (user_id, 0))
+    check_session = cur.execute(f"SELECT start_time FROM sessions WHERE user_id = (?) AND is_complete = 0", (user_id,)).fetchone()
+    if check_session:
+        await ctx.send(f"<@{user_id}> Session already active!")
         return
-    session.is_active = True
-    session.start_time = ctx.message.created_at.timestamp()
-    await ctx.send("Study session started!")
+    start_time = discord.Object(ctx.interaction.id).created_at
+    start_timestamp = start_time.timestamp()
+    cur.execute(f"INSERT INTO sessions (user_id, start_time) VALUES (?,?)", (user_id, start_timestamp))
+    con.commit()
+
+    #display time in readable format
+    human_readable_time = (start_time - datetime.timedelta(hours=5)).strftime("%H:%M:%S")
+    await ctx.send(f"<@{user_id}> New study session started at {human_readable_time}")
+
+
+@bot.slash_command(description = "End your study session and say what you did")
+async def stop(ctx, activity: str):
+    user_id = int(ctx.author.id)
+    start_value = cur.execute(f"SELECT start_time FROM sessions WHERE user_id = (?) AND is_complete = 0", (user_id,)).fetchone()
+    if not start_value:
+        await ctx.send(f"<@{user_id}> You haven't started studying yet!")
+        return
+    start_time = start_value[0]
+    duration = discord.Object(ctx.interaction.id).created_at.timestamp() - start_time
+    cur.execute(f"UPDATE sessions SET duration = (?), activity = (?), is_complete = 1 WHERE user_id = (?)", (duration, activity, user_id))
+    total_time = cur.execute(f"SELECT total_time FROM studiers WHERE user_id = (?)", (user_id,)).fetchone()[0]
+    total_time += duration
+    cur.execute(F"UPDATE studiers SET total_time = (?) WHERE user_id = (?)", (total_time, user_id))
+    con.commit()
+
+    #display time studied in readable format
+    human_readable_duration = str(datetime.timedelta(seconds=duration)).split('.')[0]
+    human_readable_total = str(datetime.timedelta(seconds=total_time)).split('.')[0]
+    await ctx.send(f"<@{user_id}> You spent {human_readable_duration} studying!\nYour total time for the week is now {human_readable_total}")
+
+
+@bot.slash_command(description = "Check the time for a user")
+async def time(ctx, user: discord.Option(discord.Member, required="True")):
+    user_id = user.id
+    if(user.nick is None):
+        user_name = user.name
+    else:
+        user_name = user.nick
+    time_value = cur.execute(f"SELECT total_time FROM studiers WHERE user_id = (?)", (user_id,)).fetchone()
+    if not time_value:
+        await ctx.send(f"{user_name} has not started studying this week.")
+        return
+    time_spent = time_value[0]
+    human_readable_time = str(datetime.timedelta(seconds=time_spent)).split('.')[0]
+    await ctx.send(f"{user_name} has {human_readable_time} study hours this week!")
+    
+
 
 ################# Extra functions #################
 
 #say hello
-@bot.command()
+@bot.slash_command(description = "Say hello")
 async def hello(ctx):
     await ctx.send("Hello!")
 
 #add variable amount of numbers
-@bot.command()
-async def add(ctx, *arr):
-    result = 0
-    for num in arr:
-        result += int(num)
+@bot.slash_command(description = "Adds two numbers")
+async def add(ctx, first: discord.Option(int), second: discord.Option(int)):
+    result = first+second
     await ctx.send(f"Result: {result}")
 
-#start bot
+
+
+###start bot
 bot.run(BOT_TOKEN)
 
 
 
 
 
-
-
-
-
-
-
-#start of slash command implementation
-'''
-#to sync slash commands (uses prefix instead of slash)
-@bot.command()
-async def sync(ctx):
-    print("sync command")
-    await bot.tree.sync(guild=discord.Object(SERVER_ID))
-    await ctx.send('Command tree synced.')
-
-@tree.command(
-    name = "hi",
-    description = "say hi"
-)
-async def hi(ctx):
-    await ctx.response.send_message("Hi!")
-'''
+#to do list
+    #function to change hours required and total_time (recognize admin)
+    #reminders as people get behind
+    #function to reset weekly? make manual or a task
+    #add comments to stop (*arr -> string -> other table referencing studier probably)
+    #the entire competition tracker
