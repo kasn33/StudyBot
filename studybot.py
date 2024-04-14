fe70660acfc8a7042c811e7de6361a79a45defea
kasn33 <112991386+kasn33@users.noreply.github.com>
2024-04-13 20:55:31 -0500

    basic admin checking and data functions

--- studybot.py
+++ studybot.py
@@ -5,10 +5,12 @@ import datetime
 import sqlite3
 
 BOT_TOKEN = ""
-CHANNEL_ID = 1227722112404553889
+CHANNEL_ID = 1174150837996625932
 SERVER_ID = 1227721401239343187
 intents = discord.Intents.all()
 client = discord.Client(intents=intents)
+DEFAULT_REQUIRED = 32400 #in seconds (9 hours)
+admin_roles = set()
 
 bot = commands.Bot(command_prefix="!", intents=intents)
 
@@ -20,9 +22,10 @@ cur = con.cursor()
 #startup message (console and channel in server)
 @bot.event
 async def on_ready():
-    cur.execute("""CREATE TABLE IF NOT EXISTS studiers(
+    cur.execute(f"""CREATE TABLE IF NOT EXISTS studiers(
         user_id INTEGER PRIMARY KEY,
-        total_time INTEGER
+        total_time INTEGER DEFAULT 0,
+        required_hours INTEGER DEFAULT {DEFAULT_REQUIRED}
     );""")
     cur.execute("""CREATE TABLE IF NOT EXISTS sessions(
         session_id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -33,7 +36,12 @@ async def on_ready():
         activity TEXT,
         FOREIGN KEY (user_id) REFERENCES studiers(user_id)
     );""")
+    cur.execute("""CREATE TABLE IF NOT EXISTS roles(
+        role_id INTEGER PRIMARY KEY,
+        purpose TEXT
+    );""")
     con.commit()
+    load_admin()
     print("Study bot is ready!")
     channel = bot.get_channel(CHANNEL_ID)
     await channel.send("Hello! Study bot is ready!")
@@ -43,15 +51,16 @@ async def on_ready():
 
 ########### Study tracker functions ###############
 
+#start studying session
 @bot.slash_command(description = "Start your study session")
 async def start(ctx):
     user_id = int(ctx.author.id)
     check_studier = cur.execute(f"SELECT total_time FROM studiers WHERE user_id = (?)", (user_id,)).fetchone()
     if not check_studier:
-        cur.execute(f"INSERT INTO studiers VALUES (?,?)", (user_id, 0))
+        cur.execute(f"INSERT INTO studiers (user_id) VALUES (?)", (user_id,))
     check_session = cur.execute(f"SELECT start_time FROM sessions WHERE user_id = (?) AND is_complete = 0", (user_id,)).fetchone()
     if check_session:
-        await ctx.send(f"<@{user_id}> Session already active!")
+        await ctx.respond(f"<@{user_id}> Session already active!")
         return
     start_time = discord.Object(ctx.interaction.id).created_at
     start_timestamp = start_time.timestamp()
@@ -60,59 +69,175 @@ async def start(ctx):
 
     #display time in readable format
     human_readable_time = (start_time - datetime.timedelta(hours=5)).strftime("%H:%M:%S")
-    await ctx.send(f"<@{user_id}> New study session started at {human_readable_time}")
+    await ctx.respond(f"<@{user_id}> New study session started at {human_readable_time}")
 
 
+#stop studying session
 @bot.slash_command(description = "End your study session and say what you did")
 async def stop(ctx, activity: str):
     user_id = int(ctx.author.id)
-    start_value = cur.execute(f"SELECT start_time FROM sessions WHERE user_id = (?) AND is_complete = 0", (user_id,)).fetchone()
-    if not start_value:
-        await ctx.send(f"<@{user_id}> You haven't started studying yet!")
+    values = cur.execute(f"SELECT start_time, session_id FROM sessions WHERE user_id = (?) AND is_complete = 0", (user_id,)).fetchone()
+    if not values:
+        await ctx.respond(f"<@{user_id}> You haven't started studying yet!")
         return
-    start_time = start_value[0]
+    start_time, sid = values
     duration = discord.Object(ctx.interaction.id).created_at.timestamp() - start_time
-    cur.execute(f"UPDATE sessions SET duration = (?), activity = (?), is_complete = 1 WHERE user_id = (?)", (duration, activity, user_id))
+    cur.execute(f"UPDATE sessions SET duration = (?), activity = (?), is_complete = 1 WHERE session_id = (?)", (duration, activity, sid))
     total_time = cur.execute(f"SELECT total_time FROM studiers WHERE user_id = (?)", (user_id,)).fetchone()[0]
     total_time += duration
-    cur.execute(F"UPDATE studiers SET total_time = (?) WHERE user_id = (?)", (total_time, user_id))
+    cur.execute(f"UPDATE studiers SET total_time = (?) WHERE user_id = (?)", (total_time, user_id))
     con.commit()
 
     #display time studied in readable format
     human_readable_duration = str(datetime.timedelta(seconds=duration)).split('.')[0]
     human_readable_total = str(datetime.timedelta(seconds=total_time)).split('.')[0]
-    await ctx.send(f"<@{user_id}> You spent {human_readable_duration} studying!\nYour total time for the week is now {human_readable_total}")
+    await ctx.respond(f"<@{user_id}> You spent {human_readable_duration} studying!\nYour total time for the week is now {human_readable_total}")
 
 
+#check how much time a user has spent studying this week
 @bot.slash_command(description = "Check the time for a user")
-async def time(ctx, user: discord.Option(discord.Member, required="True")):
+async def time(ctx, user: discord.Option(discord.Member, required="True", description="Who you want to check the hours of (could be you)")):
+    user_id = user.id
+    if not is_admin(ctx) and user_id != int(ctx.author.id):
+        await ctx.respond("You don't have permission to check other people!")
+        return
+    if(user.nick is None):
+        user_name = user.name
+    else:
+        user_name = user.nick
+    tup = cur.execute(f"SELECT total_time FROM studiers WHERE user_id = (?)", (user_id,)).fetchone()
+    if not tup:
+        await ctx.respond(f"{user_name} has not started studying this week.")
+        return
+    time_spent = tup[0]
+    human_readable_time = str(datetime.timedelta(seconds=time_spent)).split('.')[0]
+    await ctx.respond(f"{user_name} has {human_readable_time} study hours this week!")
+
+#show all sessions for a user (need permission if not that user)
+@bot.slash_command(description = "Show all of a user's activity for the week")
+async def get_report(ctx, user: discord.Option(discord.Member, required="True", description="Who you want to check the activity of (could be you)")):
+    user_id = user.id
+    if not is_admin(ctx) and user_id != int(ctx.author.id):
+        await ctx.respond("You don't have permission to check other people!")
+        return
+    if(user.nick is None):
+        user_name = user.name
+    else:
+        user_name = user.nick
+    
+    #total hours out of required
+    tup = cur.execute("SELECT total_time, required_hours FROM studiers WHERE user_id = (?)", (user_id,)).fetchone()
+    if not tup:
+        await ctx.respond(f"{user_name} has not started studing this week")
+        return
+    total_time, required_hours = tup
+    await ctx.respond(f"{user_name} has completed {str(datetime.timedelta(seconds=total_time)).split('.')[0]} out of {str(datetime.timedelta(seconds=required_hours)).split('.')[0]}")
+    
+    #session by session
+    sessions = cur.execute(f"SELECT start_time, duration, activity FROM sessions WHERE user_id = (?) AND is_complete = 1 ORDER BY start_time", (user_id,)).fetchall()
+    for tup in sessions:
+        weekday = datetime.datetime.fromtimestamp(tup[0]).strftime("%A")
+        await ctx.send(f"Session on {weekday} for {str(datetime.timedelta(seconds=tup[1])).split('.')[0]} doing: {tup[2]}")
+
+
+#remove time from a user (needs permission) (less than 24 hours)
+@bot.slash_command(description = "Add time to somebody (less than 24 hours)")
+async def add(ctx, user: discord.Option(discord.Member, required="True", description="Who you want to add time to (could be you)"), hours: discord.Option(int), minutes: discord.Option(int)):
+    if not is_admin(ctx):
+            await ctx.respond("You don't have permission for this command")
+            return
     user_id = user.id
     if(user.nick is None):
         user_name = user.name
     else:
         user_name = user.nick
-    time_value = cur.execute(f"SELECT total_time FROM studiers WHERE user_id = (?)", (user_id,)).fetchone()
-    if not time_value:
-        await ctx.send(f"{user_name} has not started studying this week.")
+    tup = cur.execute(f"SELECT total_time FROM studiers WHERE user_id = (?)", (user_id,)).fetchall()
+    if not tup:
+        await ctx.respond(f"{user_name} has not started studying this week.")
         return
-    time_spent = time_value[0]
-    human_readable_time = str(datetime.timedelta(seconds=time_spent)).split('.')[0]
-    await ctx.send(f"{user_name} has {human_readable_time} study hours this week!")
+    orig_time = tup[0]
+    new_time = orig_time + datetime.timedelta(hours=hours, minutes=minutes).seconds
+    cur.execute(f"UPDATE studiers SET total_time = (?) WHERE user_id = (?)", (new_time, user_id))
+    con.commit()
+
+    human_readable_orig = str(datetime.timedelta(seconds=orig_time)).split('.')[0]
+    human_readable_new = str(datetime.timedelta(seconds=new_time)).split('.')[0]
+    await ctx.respond(f"Changed hours for {user_name} from {human_readable_orig} to {human_readable_new}")
+
+
+#add time to a user (needs permission) (less than 24 hours)
+@bot.slash_command(description = "Remove time from somebody (less than 24 hours)")
+async def subtract(ctx, user: discord.Option(discord.Member, required="True", description="Who you want to remove time from (could be you)"), hours: discord.Option(int), minutes: discord.Option(int)):
+    if not is_admin(ctx):
+        await ctx.respond("You don't have permission for this command")
+        return
+    user_id = user.id
+    if(user.nick is None):
+        user_name = user.name
+    else:
+        user_name = user.nick
+    tup = cur.execute(f"SELECT total_time FROM studiers WHERE user_id = (?)", (user_id,)).fetchone()
+    if not tup:
+        await ctx.respond(f"{user_name} has not started studying this week.")
+        return
+    orig_time = tup[0]
+    new_time = orig_time - datetime.timedelta(hours=hours, minutes=minutes).seconds
+    cur.execute(f"UPDATE studiers SET total_time = (?) WHERE user_id = (?)", (new_time, user_id))
+    con.commit()
+
+    human_readable_orig = str(datetime.timedelta(seconds=orig_time)).split('.')[0]
+    human_readable_new = str(datetime.timedelta(seconds=new_time)).split('.')[0]
+    await ctx.respond(f"Changed hours for {user_name} from {human_readable_orig} to {human_readable_new}")
+
+
+#add roles for admin (must be server administrator)
+@bot.slash_command(description = "Add admin permissions to a role (must be server owner)")
+async def promote(ctx, role: discord.Option(discord.Role, required="True", description="Which role do you want to give admin permissions to")):
+    if not is_admin(ctx) and not ctx.author.guild_permissions.administrator:
+        await ctx.respond("You don't have permission for this command")
+        return
+    if role.id in admin_roles:
+        await ctx.respond("Role is already admin")
+        return
+    cur.execute(f"INSERT INTO roles (role_id, purpose) VALUES (?, ?)", (role.id, "ADMIN"))
+    con .commit()
+    admin_roles.add(role.id)
+    await ctx.respond(f"Added role {role.name} as admin")
+
+#clear all the data to start a fresh week
+@bot.slash_command(description = "Clear all data for the week (BE CAREFUL)")
+async def clear(ctx, sure: discord.Option(str, description="Type \"Yes I am sure\" if you're sure")):
+    if not is_admin(ctx):
+        await ctx.respond("You don't have permission for this command")
+        return
+    if sure != "Yes I am sure":
+        await ctx.respond("Make sure you're sure")
+        return
+    cur.execute(f"UPDATE studiers SET total_time = 0")
+    cur.execute(f"DELETE FROM sessions")
+    con.commit()
+    await ctx.respond("Weekly data reset!")
     
 
 
-################# Extra functions #################
+################# Extra functions and helpers #################
 
 #say hello
 @bot.slash_command(description = "Say hello")
 async def hello(ctx):
-    await ctx.send("Hello!")
+    await ctx.respond("Hello!")
 
-#add variable amount of numbers
-@bot.slash_command(description = "Adds two numbers")
-async def add(ctx, first: discord.Option(int), second: discord.Option(int)):
-    result = first+second
-    await ctx.send(f"Result: {result}")
+def is_admin(ctx):
+    for role in ctx.author.roles:
+        if role.id in admin_roles:
+            return True
+    return False
+
+def load_admin():
+    admin_tups = cur.execute(f"SELECT role_id FROM roles WHERE purpose = \"ADMIN\"").fetchall()
+    for tup in admin_tups:
+        admin_roles.add(tup[0])
+
 
 
 
@@ -124,8 +249,9 @@ bot.run(BOT_TOKEN)
 
 
 #to do list
-    #function to change hours required and total_time (recognize admin)
     #reminders as people get behind
-    #function to reset weekly? make manual or a task
-    #add comments to stop (*arr -> string -> other table referencing studier probably)
+    #auto reset?
+    #weekly report
+    #check for active
+    #list by day
     #the entire competition tracker
